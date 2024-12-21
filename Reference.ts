ff4100b315f351603596706d5e512fa4e5181a9c
Thi Nguyen <anhthi.ieig@gmail.com>
2024-12-21 10:09:35 +0700

    Moving codebase

---
+++ Reference.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+const requiredMetadataKey = Symbol("required");
+
+function required(
+  target: Object,
+  propertyKey: string | symbol,
+  parameterIndex: number
+) {
+  let existingRequiredParameters: number[] =
+    Reflect.getOwnMetadata(requiredMetadataKey, target, propertyKey) || [];
+  existingRequiredParameters.push(parameterIndex);
+  Reflect.defineMetadata(
+    requiredMetadataKey,
+    existingRequiredParameters,
+    target,
+    propertyKey
+  );
+}
